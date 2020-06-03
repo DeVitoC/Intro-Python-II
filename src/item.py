8e62be4754311a4827077307008ac81ff854f4cf
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-03 13:53:37 -0400

    Added item.py

---
+++ src/item.py
@@ -0,0 +1,7 @@
+# A class that defines the items that are in rooms and that players can pick up
+
+class Item:
+    
+    def __init_(self, name, description):
+        self.name = name
+        self.description = description
